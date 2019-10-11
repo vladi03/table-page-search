3af3d6b50d8e842269189cd95df00da1fd105b0d
vmartinez <vladimir.martinez@bcdtravel.com>
2019-10-11 09:35:58 -0400

    base configuration. good template

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import './index.css'
+import React from 'react'
+import {render} from 'react-dom'
+import {TableDemo} from "./tables";
+
+render(<TableDemo />, document.querySelector('#app'));
+
---
+++ src/tables/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const TableDemo = () => {
+
+    return (
+        <div>
+            Hi World
+        </div>
+    );
+};
+
