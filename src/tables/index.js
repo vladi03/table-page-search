3f0be3a553a304dc5f8b141321500dad85696900
vmartinez <vladimir.martinez@bcdtravel.com>
2019-10-11 10:13:34 -0400

    fix build command

--- src/tables/index.js
+++ src/tables/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 
-export const TableDemo = () => {
-
-    return (
+export const TableDemo = () => (
+    <React.Fragment>
         <div>
             Hi World
         </div>
-    );
-};
-
+        <div>
+            Template
+        </div>
+    </React.Fragment>
+);
